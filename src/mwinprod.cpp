edb4c49068f2fb1b70ef3aea7bc71641ab0a2e78
Hossein Haghbin <haghbin@pgu.ac.ir>
2022-09-08 20:12:54 +0000

    version 2.1.0

--- src/mwinprod.cpp
+++ src/mwinprod.cpp
@@ -13,17 +13,17 @@ double mwinprod(std::vector<arma::mat> X, std::vector<arma::mat> Y,arma::vec w,s
   for(int j=0; j<p; j++)
   {
 
-    x=X[j];
-    y=Y[j];
-    g=G[j];
-    int N = x.n_cols;
-    arma::vec xi(x.n_rows),yi(y.n_rows);
-    for(int i=0; i<N; i++ )
-    {
-      xi = x.col(i);
-      yi = y.col(i);
-      s += arma::as_scalar(xi.t()*yi*w(i));
-    }
+      x=X[j];
+      y=Y[j];
+      g=G[j];
+      int N = x.n_cols;
+      arma::vec xi(x.n_rows),yi(y.n_rows);
+      for(int i=0; i<N; i++ )
+      {
+        xi = x.col(i);
+        yi = y.col(i);
+        s += arma::as_scalar(xi.t()*yi*w(i));
+      }
   }
   return(s);
 }
